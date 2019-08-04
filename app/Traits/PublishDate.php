cd4080987b4d2f6a8fe62eabc7812e3a46981ddf
abraham ghaemi <ig.ghaemi@gmail.com>
2019-08-05 02:47:33 +0430

    add date tratis

---
+++ app/Traits/PublishDate.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace App\Traits;
+
+trait PublishDate 
+{
+	public function normalizeDate($bad_date)
+	{
+		$better_date = str_replace('تاریخ انتشار  ', "", $bad_date);
+		$split_date = explode(' ', $better_date);
+
+
+		$farsi_chars = [
+			'فروردین','اردیبهشت','خرداد','تیر','مرداد','شهریور','مهر','آبان','آذر','دی', 'بهمن', 'اسفند'
+		];
+		$farsi_chars = [
+			'۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'
+		];
+		$latin_chars = [
+			'0', '1','2','3','4','5','6','7','8','9'
+		];
+
+		$month = str_replace(
+			$farsi_chars, $latin_chars, trim($split_date[1])
+		);
+
+
+
+		$day = str_replace(
+			$farsi_chars, $latin_chars, $split_date[0]
+		);
+		$year = str_replace(
+			$farsi_chars, $latin_chars, $split_date[2]
+		);
+
+		$date = new \jDateTime(true, true, 'Asia/Tehran');
+		$time = $date->mktime(0,0,0,$month, $day,$year);
+		
+		return gmdate("Y-m-d", $time);		
+	}
+
+}
