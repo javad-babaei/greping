ab435b31e68b6fccda3ceacb58104c3c52b33277
abraham ghaemi <ig.ghaemi@gmail.com>
2019-08-05 03:17:59 +0430

    add greptor artist with tracks

---
+++ app/Console/Commands/GreptorArtistWithTrack.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Grep\Batch\Artist;
+use App\Grep\Batch\Ahaang;
+use App\Greptor as model;
+
+class GreptorArtistWithTrack extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'grep:mp3';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'grep one artist with track';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $request = Model::where('done', 0)->where('type', 'artist')->first();
+
+        if(!$request) {
+            return false;
+        }
+
+        
+
+
+        try {
+
+            $artist = new Artist($request->link);
+            $artist->add();
+            $track_links = $artist->getTrackLink();
+
+            foreach ($track_links as $track_link) {
+                dump($track_link);
+
+                if($track_link){
+                    (
+                        new Ahaang($track_link)
+                    )->proccess();
+                }
+            }
+
+            $request->done = true;
+            $request->save();
+
+        } catch (Exception $e) {
+
+            $message = "Error Processing Request";
+            $request->description = $message . $e->getMessage();
+            $request->done = true;
+            $request->save();
+
+            throw new Exception($message, 1);
+        }
+        
+    }
+}
