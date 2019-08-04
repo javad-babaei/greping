9a9641d6b60b34100bd94fb4afedeb9af95589cd
abraham ghaemi <ig.ghaemi@gmail.com>
2019-08-05 02:55:11 +0430

    fix date convertor

--- app/Traits/PublishDate.php
+++ app/Traits/PublishDate.php
@@ -13,25 +13,28 @@ trait PublishDate
 		$farsi_chars = [
 			'فروردین','اردیبهشت','خرداد','تیر','مرداد','شهریور','مهر','آبان','آذر','دی', 'بهمن', 'اسفند'
 		];
-		$farsi_chars = [
-			'۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'
-		];
-		$latin_chars = [
-			'0', '1','2','3','4','5','6','7','8','9'
-		];
+		$latin_chars = [1,2,3,4,5,6,7,8,9, 10, 11, 12];
+
 
 		$month = str_replace(
 			$farsi_chars, $latin_chars, trim($split_date[1])
 		);
 
 
-
+		$farsi_chars = [
+			'۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'
+		];
+		$latin_chars = [
+			'0', '1','2','3','4','5','6','7','8','9'
+		];
 		$day = str_replace(
 			$farsi_chars, $latin_chars, $split_date[0]
 		);
 		$year = str_replace(
 			$farsi_chars, $latin_chars, $split_date[2]
 		);
+
+
 
 		$date = new \jDateTime(true, true, 'Asia/Tehran');
 		$time = $date->mktime(0,0,0,$month, $day,$year);
