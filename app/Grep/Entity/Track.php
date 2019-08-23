95bdc51a3b0120947fe84128a6da104e5b495c39
abraham ghaemi <ig.ghaemi@gmail.com>
2019-08-24 01:26:12 +0430

    track must check artist before title

--- app/Grep/Entity/Track.php
+++ app/Grep/Entity/Track.php
@@ -18,10 +18,14 @@ class Track extends Api
 
 	public function trackExists($data)
 	{
+
 		return $this->client()->request('GET', 'Track', [
 			'where[0][type]' => 'startsWith',
 			'where[0][attribute]' => 'translate',
-			'where[0][value]' => $data['translate']
+			'where[0][value]' => $data['translate'],
+			'where[1][type]' => 'startsWith',
+			'where[1][attribute]' => 'artist',
+			'where[1][value]' => $data['artist']
 		])['total'];
 	}
 	public function findByName($name)
