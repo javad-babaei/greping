59f716b61638b84c3a64390be8bcf7ce58c14f1d
devops <devops@localhost.localdomain>
2019-11-11 10:17:39 +0330

    Error/Exception handling on upload track migration to track

--- app/Console/Commands/UploadTrackMoveTrack.php
+++ app/Console/Commands/UploadTrackMoveTrack.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use \App\Grep\Entity\Api;
 use Illuminate\Console\Command;
+use \Illuminate\Database\QueryException;
+use \FFMpeg\FFProbe;
 
 class UploadTrackMoveTrack extends Command
 {
@@ -37,53 +40,92 @@ class UploadTrackMoveTrack extends Command
      */
     public function handle()
     {
-        $list = \DB::table('upload_track')->where('deleted', 0)->get();
 
-        $path = "/home/app/music/repository/";
+        try {
+            $list =
+                \DB::table( 'upload_track' )
+                    ->where( 'deleted', 0 )
+                    ->get();
+        }
+        catch ( QueryException $e ) {
+            dd( $e->getMessage() );
+        }
+
+        $api_client = ( new Api() )->client();
+        $success_ids = $failed_to_copy_file = $failed_to_request = $data = [];
+
+        $path        = "/home/app/music/repository/";
         $upload_path = "/home/app/music/api/data/upload/";
 
-        $data = [];
+        foreach ( $list as $track ) {
 
-        foreach ($list as $track) {
-            $mp3_id = $track->file_id;
-            $cover_id = $track->cover_id;
+            if ( isset( $track->file_id ) && isset( $track->cover_id ) ) {
 
-            $mp3_file = $upload_path . $mp3_id;
-            $cover_file = $upload_path . $cover_id;
+                $mp3_id   = $track->file_id;
+                $cover_id = $track->cover_id;
 
-            $filesource = $path . "track/" . $mp3_id . ".mp3";
-            $coversource = $path . "cover/" . $cover_id . ".jpg";
-	    dump($coversource, $cover_file);
-	    dump($filesource, $mp3_file);
-	    exec('cp ' . $cover_file . " " . $coversource);
-	    exec('cp ' . $mp3_file . " " . $filesource);
+                $mp3_file   = $upload_path . $mp3_id;
+                $cover_file = $upload_path . $cover_id;
 
-            //file_put_contents($filesource, base64_decode($mp3_file));
-            //file_put_contents($coversource, base64_decode($cover_id));
+                $file_source  = $path . "track/" . $mp3_id   . ".mp3";
+                $cover_source = $path . "cover/" . $cover_id . ".jpg";
 
-            $ffprobe = \FFMpeg\FFProbe::create();
-            @$duration =  @$ffprobe->format($filesource)->get('duration');
-            $duration ?? 0;
-		dump($duration);
+                unset( $result, $output );
 
-            $data['stream'] = "/track/stream/" . $mp3_id . ".aac";
-            $data['trackUrl'] = "/track/" . $mp3_id . ".mp3";
-            $data['img'] = "/cover/" . $cover_id . ".jpg";
-            $data['segmentlist'] = "/track/segment/" . $mp3_id . "/track.m3u8";
-            $data['name'] = $track->name;
-            //$data['artist'] = $track->artist_name;
-            $data['description'] = $track->description;
-            $data['lyric'] = $track->description;
-            $data['genres'] = $track->genres;
-            $data['publishedDate'] = $track->published_date;
-            $data['duration'] = $duration;
-	    $data['assignedUserId'] = 1;
-	    $data['assignedUserName'] = 'root';
+                exec( 'cp ' . $mp3_file   . " " . $file_source,  $output, $result );
+                exec( 'cp ' . $cover_file . " " . $cover_source, $output, $result );
 
-            $api = new \App\Grep\Entity\Api;
-            $newTrack = $api->client()->request('POST', 'track', $data);
+                if ( $result === 0 ) {
 
-            dd($newTrack);
+                    $ffprobe = FFProbe::create();
+                    @$duration =  @$ffprobe->format( $file_source )->get( 'duration' );
+                    $duration = $duration ?? 0;
+
+                    $data[ 'img'              ] = "/cover/" . $cover_id . ".jpg";
+                    $data[ 'stream'           ] = "/track/stream/" . $mp3_id . ".aac";
+                    $data[ 'trackUrl'         ] = "/track/" . $mp3_id . ".mp3";
+                    $data[ 'segmentlist'      ] = "/track/segment/" . $mp3_id . "/track.m3u8";
+                    $data[ 'name'             ] = isset( $track->name ) ? $track->name : '';
+                    $data[ 'lyric'            ] = isset( $track->description ) ? $track->description : '';
+                    $data[ 'description'      ] = $data[ 'lyric' ];
+                    $data[ 'genres'           ] = isset( $track->genres ) ? $track->genres : '';
+                    $data[ 'publishedDate'    ] = isset( $track->published_date ) ? $track->published_date : '';
+                    $data[ 'duration'         ] = $duration;
+                    $data[ 'assignedUserId'   ] = 1;
+                    $data[ 'assignedUserName' ] = 'root';
+
+                    try {
+                        $new_track = $api_client->request( 'POST', 'track', $data );
+                        $success_ids[] = $track->id;
+                    }
+                    catch ( \Exception $e ) {
+                        $failed_to_request[] =
+                            [
+                                'track_id' => $track->id,
+                                'request_exception' => $e->getMessage()
+                            ];
+                    }
+                } else {
+                    $failed_to_copy_file[] = $track->file_id;
+                }
+            }
         }
+
+        if ( $success_ids ) {
+            try {
+                \DB::table( 'upload_track' )
+                    ->whereIn( 'id', $success_ids )
+                    ->update( 'deleted', 1 );
+            }
+            catch ( QueryException $e ) {
+                dd( $e->getMessage() ); 
+            }
+        }
+
+        $this->info(
+            'Succeeded tracks to move : '    . count( $success_ids         ) . PHP_EOL .
+            'Failed tracks to request : '    . count( $failed_to_request   ) . PHP_EOL .
+            'Failed tracks to copy files : ' . count( $failed_to_copy_file )
+        );
     }
 }
